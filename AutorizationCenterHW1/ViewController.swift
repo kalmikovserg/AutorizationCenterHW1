58d543cac9bc3f99c2042ede71016a412156e358
kalmykov1981 <kalmikovw13@gmail.com>
2019-06-23 15:30:16 -0500

    Initial Commit

---
+++ AutorizationCenterHW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutorizationCenterHW1
+//
+//  Created by Сергей Калмыков on 6/23/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
